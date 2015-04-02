0e2cd0b4daefa8b33f76da00f64b7e9100b8f0d4
sos <sos@SorasakNotebook>
2015-04-02 12:30:54 +0700

    sos

--- frontend/controllers/EpiController.php
+++ frontend/controllers/EpiController.php
@@ -26,13 +26,13 @@ select  h.hoscode as hospcode ,h.hosname as hospname,
 (SELECT hos_target from
  (select person.hospcode , count(distinct person.pid) as hos_target from person  
            where person.discharge = '9' and person.typearea in ('1', '3') and person.nation ='099' 
-           and (person.birth BETWEEN DATE_ADD('$date1',INTERVAL -71 month) and DATE_ADD('$date2',INTERVAL -71 month)) group by person.hospcode ) as t
+           and (person.birth BETWEEN '$date1' and '$date2') group by person.hospcode ) as t
 where  t.hospcode = h.hoscode
 ) as target ,
 (SELECT hos_doit from
           (select person.hospcode,count(distinct(person.pid)) as hos_doit from epi  inner join person on epi.hospcode = person.hospcode and epi.pid = person.pid 
            where person.discharge = '9' and person.typearea in ('1', '3') and person.nation ='099'  
-           and (person.birth BETWEEN DATE_ADD('$date1',INTERVAL -71 month) and DATE_ADD('$date2',INTERVAL -71 month))  and epi.VACCINETYPE = '035'  group by person.hospcode) as r
+           and (person.birth BETWEEN '$date1' and '$date2')  and epi.VACCINETYPE = '035'  group by person.hospcode) as r
 where r.hospcode = h.hoscode
 ) as result 
 
