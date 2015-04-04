9c40b56c7b046357dbc26b9339090ee1aaa499f8
admin <admin@PC-Server>
2015-04-04 19:58:40 +0700

    ปรับปรุงการแสดงผลหน้า index หมวดรายงาน ทีมหมอครอบครัว 2015-04-4

--- frontend/views/kukks/index.php
+++ frontend/views/kukks/index.php
@@ -15,41 +15,32 @@
     <div class="panel-heading">
         ทีมหมอครอบครัว
     </div>
-    <div class="panel-body">
+    <div class="list-group">
 
-        <p>
-            <?php
-            echo \yii\helpers\Html::a('1) สัดส่วนบุคลากรสาธารณสุขต่อประชากร ', ['kukks/report2']).' <span class="label label-success">update 04-04-58</span>';
-            ?>
-        </p>
-
-        <p>
-            <?php
-            echo \yii\helpers\Html::a('2) จำนวน อสม.ต่อหลังคาเรือน', ['kukks/report3']).' <span class="label label-success">update 04-04-58</span>';
-            ?>
-        </p>
+        <?php
+        echo \yii\helpers\Html::a('1) สัดส่วนบุคลากรสาธารณสุขต่อประชากร  <span class="label label-success">update 04-04-58</span> ', ['kukks/report2'], ['class' => 'list-group-item']);
+        ?>
+        <?php
+        echo \yii\helpers\Html::a('2) จำนวน อสม.ต่อหลังคาเรือน <span class="label label-success">update 04-04-58</span> ', ['kukks/report3'], ['class' => 'list-group-item']);
+        ?>
     </div>
+
     <div class="panel-heading">
         เยี่ยมบ้าน
     </div>
-        <div class="panel-body">
-        <p>
-            <?php
-            echo \yii\helpers\Html::a('1) ผู้ป่วยโรคเรื้อรังได้รับการเยี่ยมบ้าน', ['kukks/report1']).' <span class="label label-success">update 04-04-58</span>';
-            ?>
-        </p>
+    <div class="list-group">
 
+        <?php
+        echo \yii\helpers\Html::a('1) ผู้ป่วยโรคเรื้อรังได้รับการเยี่ยมบ้าน <span class="label label-success">update 04-04-58</span> ', ['kukks/report1'], ['class' => 'list-group-item']);
+        ?>
 
-        <p>
-            <?php
-            echo \yii\helpers\Html::a('2) ผู้สูงอายุ60ปีขึ้นไปได้รับการเยี่ยมบ้าน', ['kukks/report4']).' <span class="label label-success">update 04-04-58</span>';
-            ?>
-        </p>
-        <p>
-            <?php
-            echo \yii\helpers\Html::a('3) หญิงตั้งครรภ์ที่ได้รับการเยี่ยมบ้าน', ['kukks/report5']).' <span class="label label-success">update 04-04-58</span>';
-            ?>
-        </p>
+        <?php
+        echo \yii\helpers\Html::a('2) ผู้สูงอายุ60ปีขึ้นไปได้รับการเยี่ยมบ้าน <span class="label label-success">update 04-04-58</span> ', ['kukks/report4'], ['class' => 'list-group-item']);
+        ?>
+
+        <?php
+        echo \yii\helpers\Html::a('3) หญิงตั้งครรภ์ที่ได้รับการเยี่ยมบ้าน <span class="label label-success">update 04-04-58</span> ', ['kukks/report5'], ['class' => 'list-group-item']);
+        ?>
     </div>
 
 </div>
