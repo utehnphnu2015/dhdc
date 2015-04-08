8f28bdbbd60d2eba5be6b81ead21decb956e8afe
DHDC Main <utehnphnu2015@gmail.com>
2015-04-08 22:46:12 +0700

    tehnn,20150408

--- backend/views/execute/index.php
+++ backend/views/execute/index.php
@@ -7,7 +7,7 @@ use yii\widgets\Pjax;
 
 <?php Pjax::begin(); ?>
 <?= Html::a("Refresh", ['execute/index'], ['class' => 'btn btn-lg btn-primary', 'id' => 'refreshButton']) ?>
-<p>Server Time: <span style="background-color: white"><?= $time ?></span></p>
+<p>Server Time: <span style="background-color: white" id="divtoBlink"><?= $time ?></span></p>
 <p>
     Current Process: <span style="background-color: white"><?=$fnc_name?></span>
     Start Time: <span style="background-color: white"><?=$fnc_time?></span>
@@ -40,6 +40,14 @@ $script = <<< JS
 $(document).ready(function() {
     setInterval(function(){ $("#refreshButton").click(); }, 5000);
 });
+
+setInterval(function () {
+    $("#divtoBlink").css("background-color", function () {
+        this.switch = !this.switch
+        return this.switch ? "orange" : "white"
+    });
+}, 1000)
+
 JS;
 $this->registerJs($script);
 ?>
