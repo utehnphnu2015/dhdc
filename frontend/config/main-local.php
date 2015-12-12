a76557cdfedce48125e5048e29ace1d269f2894e
tehnn <tehnnn@gmail.com>
2015-12-12 23:34:07 +0700

    dev-1.0

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -4,15 +4,15 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'J8GDRYQyYQScQDA6_e7n2LimAzdfuhau',
+            'cookieValidationKey' => 'zqv0r-fVImc4m9j1MEkmI-UIswX53tXG',
         ],
     ],
 ];
 
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
-    //$config['bootstrap'][] = 'debug';
-    //$config['modules']['debug'] = 'yii\debug\Module';
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = 'yii\gii\Module';
